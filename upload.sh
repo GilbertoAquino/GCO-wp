d0e3c7fc0be857f2b60697de30c850beb772cb69
Gilberto Aquino <gaquinos@outlook.com>
2023-04-29 02:33:43 +0000

    Modify upload.sh

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 USER="jenkins-nhs@gcorneayoftalmologia.mx"
 PASSWORD="H*pmt{jz~(2D"
 HOST="www.gcorneayoftalmologia.mx"
-ftp ftp -inv $HOST <<EOF
+ftp -inv $HOST <<EOF
 user $USER $PASSWORD
 ls
 bye
